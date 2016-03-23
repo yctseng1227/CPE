44301a0238325a4c82c4aa278fa2f1882f228242
Geneyozakura <gene26620560@gmail.com>
2016-03-24 00:55:47 +0800

    Update UVa10050 - Hartals.cpp

--- CPE_1_Star_Problems/UVa10050 - Hartals.cpp
+++ CPE_1_Star_Problems/UVa10050 - Hartals.cpp
@@ -1,21 +1,21 @@
 /*==========================================
 UVa10050 - Hartals
 ============================================
-Completion Date: 105/01/21	by Geneyozakura
+Completion Date: 105/01/21   by Geneyozakura
 ==========================================*/
 #include <iostream>
 using namespace std;
 int main()
 {
-int iCase;					//儲存資料組個數
-int iDay;					//儲存要測試天數的範圍
-int iParty;					//儲存政黨個數
-int iHartalBegin[100];		//儲存各政黨不工作的起始天
-int iTotal;					//儲存不工作的總天數
-bool bHartalDays[3650];		//判斷該天是否要工作
-int i;						//迴圈變數
-int j;						//迴圈變數
-int k;						//迴圈變數
+int iCase;				//儲存資料組個數
+int iDay;				//儲存要測試天數的範圍
+int iParty;				//儲存政黨個數
+int iHartalBegin[100];			//儲存各政黨不工作的起始天
+int iTotal;				//儲存不工作的總天數
+bool bHartalDays[3650];			//判斷該天是否要工作
+int i;					//迴圈變數
+int j;					//迴圈變數
+int k;					//迴圈變數
 
 //輸入資料組個數
 cin >> iCase;
