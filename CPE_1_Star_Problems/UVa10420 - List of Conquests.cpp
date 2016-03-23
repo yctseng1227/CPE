c4751fc19117f1c387eed8568298de6c494ed5b5
Geneyozakura <gene26620560@gmail.com>
2016-03-24 01:15:49 +0800

    Update UVa10420 - List of Conquests.cpp

--- CPE_1_Star_Problems/UVa10420 - List of Conquests.cpp
+++ CPE_1_Star_Problems/UVa10420 - List of Conquests.cpp
@@ -1,19 +1,19 @@
 /*==========================================
 UVa10420 - List of Conquests
 ============================================
-Completion Date: 105/02/23  by Geneyozakura
+Completion Date: 105/02/23   by Geneyozakura
 ==========================================*/
 #include <iostream>
 #include <string>
 using namespace std;
 int main()
 {
-int iCase;					//儲存測資個數
+int iCase;			//儲存測資個數
 char cCountry[2000][10];	//儲存國家名
-string sName;				//儲存人名
-int iCount;					//儲存國家出現次數
-int i;						//迴圈變數
-int j;						//迴圈變數
+string sName;			//儲存人名
+int iCount;			//儲存國家出現次數
+int i;				//迴圈變數
+int j;				//迴圈變數
 
 //依題意輸入
 cin >> iCase;
