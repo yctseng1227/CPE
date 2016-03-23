7e72dac8b28942dea5f4c2f41ce3f9244dbfb616
Geneyozakura <gene26620560@gmail.com>
2016-03-24 00:51:57 +0800

    Update UVa00490 - Rotating Sentences.cpp

--- CPE_1_Star_Problems/UVa00490 - Rotating Sentences.cpp
+++ CPE_1_Star_Problems/UVa00490 - Rotating Sentences.cpp
@@ -1,20 +1,20 @@
 /*==========================================
 UVa00490 - Rotating Sentences
 ============================================
-Completion Date: 105/02/20	by Geneyozakura
+Completion Date: 105/02/20   by Geneyozakura
 ==========================================*/
 #include <iostream>
 #include <string>
 using namespace std;
 int main()
 {
-string sInput;					//儲存 輸入的字串
+string sInput;				//儲存 輸入的字串
 int iInputLength[100];			//儲存 輸入字串的長度
-int iMaxLength;					//儲存 儲存輸入字串中最長的長度
+int iMaxLength;				//儲存 儲存輸入字串中最長的長度
 char cStoreInput[100][100];		//儲存 每次輸入的字串
-int iCount;						//儲存 計算輸入字串的次數
-int i;							//迴圈變數
-int j;							//迴圈變數
+int iCount;				//儲存 計算輸入字串的次數
+int i;					//迴圈變數
+int j;					//迴圈變數
 
 //初始化
 iCount = 0;
